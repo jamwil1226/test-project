cc547fcc692a1b9d4a5480c1561d3481c8637350
Jamie Williams <jamwil1226@yahoo.com>
2021-11-27 15:15:51 -0500

    code refactor

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,21 +1,21 @@
-const msg = () => {
-    if(err) {
-        res.render('./layouts/main.handlebars', {
-            msg: err
-        });
-    } else {
-        if(req.file === undefined){
-            res.render('./layouts/main.handlebars', {
-                msg: 'Error: No File Selected!'
-            });
-        } else {
-            res.render('./layouts/main.handlebars', {
-                msg: 'File Uploaded!',
-                file: `uploads/${req.file.filename}`
-            });
-        }
-    }
-};
+// const msg = () => {
+//     if(err) {
+//         res.render('./layouts/main.handlebars', {
+//             msg: err
+//         });
+//     } else {
+//         if(req.file === undefined){
+//             res.render('./layouts/main.handlebars', {
+//                 msg: 'Error: No File Selected!'
+//             });
+//         } else {
+//             res.render('./layouts/main.handlebars', {
+//                 msg: 'File Uploaded!',
+//                 file: `uploads/${req.file.filename}`
+//             });
+//         }
+//     }
+// };
 
 const upload = () => {
     if(err) {
@@ -40,4 +40,4 @@ const upload = () => {
     }
 };
 
-module.exports = msg, upload;
+module.exports = upload;
